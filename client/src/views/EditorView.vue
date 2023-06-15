a49dba1af6ca3bd9ce8b9c296ac69a0a03465ec5
Sergey Grinevich <greenew.sergei@gmail.com>
2023-06-15 13:30:51 +0300

    added better article editor

--- client/src/views/EditorView.vue
+++ client/src/views/EditorView.vue
@@ -1,6 +1,35 @@
 <template>
   <div class="article-editor">
     <h2 class="editor-title">Article Editor</h2>
+    <div class="title-container">
+      <label for="title" class="label">Title:</label>
+      <input type="text" id="title" v-model="title" class="input" />
+    </div>
+    <div class="categories-container">
+      <label class="label">Categories:</label>
+      <div class="category-checkboxes">
+        <label v-for="category in availableCategories" :key="category" class="category-label">
+          <input
+            type="checkbox"
+            :value="category"
+            v-model="selectedCategories"
+            class="category-checkbox"
+          />
+          {{ category }}
+        </label>
+      </div>
+    </div>
+    <div class="tags-container">
+      <label for="tags" class="label">Tags:</label>
+      <div class="tag-selection">
+        <select id="tags" v-model="selectedTags" multiple class="tag-select">
+          <option v-for="tag in filteredTags" :key="tag">{{ tag }}</option>
+        </select>
+      </div>
+      <div class="added-tags">
+        <span class="tag" v-for="(tag, index) in tags" :key="index">{{ tag }}</span>
+      </div>
+    </div>
     <div class="editor-content" ref="editorContent"></div>
     <div class="button-container">
       <button class="publish-button" @click="publish">Publish</button>
@@ -18,6 +47,35 @@ export default {
   mounted() {
     this.initializeEditor()
   },
+  data() {
+    return {
+      editor: null,
+      content: '',
+      title: '',
+      availableCategories: [
+        'Tech',
+        'Programming',
+        'Web Development',
+        'Artificial Intelligence',
+        'Cybersecurity'
+      ],
+      selectedCategories: [],
+      selectedTags: [],
+      categoryTags: {
+        Tech: ['Tech'],
+        Programming: ['Programming'],
+        'Web Development': ['Web Development'],
+        'Artificial Intelligence': ['Artificial Intelligence'],
+        Cybersecurity: ['Cybersecurity']
+      },
+      tags: []
+    }
+  },
+  computed: {
+    filteredTags() {
+      return this.selectedCategories.flatMap((category) => this.categoryTags[category] || [])
+    }
+  },
   methods: {
     initializeEditor() {
       this.editor = new Quill(this.$refs.editorContent, {
@@ -32,16 +90,14 @@ export default {
     publish() {
       // Add publish logic here
       console.log('Publishing article...')
+      console.log('Title:', this.title)
+      console.log('Categories:', this.selectedCategories)
+      console.log('Tags:', this.tags)
+      console.log('Content:', this.content)
     },
     cancel() {
       // Add cancel logic here
       console.log('Canceling article...')
-    }
-  },
-  data() {
-    return {
-      editor: null,
-      content: ''
     }
   }
 }
@@ -64,6 +120,77 @@ export default {
   text-align: center;
   font-size: 24px;
   margin-bottom: 20px;
+}
+
+.title-container {
+  display: flex;
+  align-items: center;
+  margin-bottom: 10px;
+}
+
+.label {
+  margin-right: 10px;
+}
+
+.input {
+  padding: 5px;
+  border-radius: 4px;
+  border: 1px solid #ccc;
+}
+
+.categories-container {
+  display: flex;
+  align-items: center;
+  margin-bottom: 10px;
+}
+
+.category-checkboxes {
+  display: flex;
+  flex-wrap: wrap;
+}
+
+.category-label {
+  margin-right: 10px;
+  font-size: 14px;
+  cursor: pointer;
+}
+
+.category-checkbox {
+  margin-right: 5px;
+}
+
+.tags-container {
+  display: flex;
+  align-items: center;
+  margin-bottom: 10px;
+}
+
+.tag-selection {
+  flex-grow: 1;
+  margin-right: 10px;
+}
+
+.tag-select {
+  width: 100%;
+  padding: 5px;
+  border-radius: 4px;
+  border: 1px solid #ccc;
+}
+
+.added-tags {
+  display: flex;
+  flex-wrap: wrap;
+  margin-top: 5px;
+}
+
+.tag {
+  display: inline-block;
+  padding: 2px 8px;
+  margin-right: 5px;
+  margin-bottom: 5px;
+  font-size: 14px;
+  background-color: #e0e0e0;
+  border-radius: 4px;
 }
 
 .editor-content {
